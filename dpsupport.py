5cb6b5489b815a4578f34a4520c57d883765e17d
Sergii Mozgovyi <mozgovyi@gmail.com>
2017-11-07 13:57:19 +0200

    echo-bot

--- dpsupport.py
+++ dpsupport.py
@@ -1,11 +1,15 @@
 import constants
-import requests
+import telebot
 
 
-def start():
-    pass
+bot = telebot.TeleBot(constants.TOKEN_TG)
+
+
+@bot.message_handler(content_types=['text'])
+def repeat_all_message(message):
+
+    bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
-    start()
-
+    bot.polling(none_stop=True)
